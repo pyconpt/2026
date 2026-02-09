c71588170dc02cded4597d97ce594f2d97ec897e
Felipe Valverde <fevalverde101@gmail.com>
2026-02-09 12:53:19 -0300

    Change staticfiles storage

--- config/settings/production.py
+++ config/settings/production.py
@@ -57,7 +57,7 @@ SECURE_CONTENT_TYPE_NOSNIFF = env.bool("DJANGO_SECURE_CONTENT_TYPE_NOSNIFF", def
 
 # STATIC
 # ------------------------
-STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
+STATICFILES_STORAGE = "whitenoise.storage.CompressedStaticFilesStorage"
 # MEDIA
 # ------------------------------------------------------------------------------
 
